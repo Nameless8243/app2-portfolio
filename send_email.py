1197826171838caf5d3c96d9ba74e6b37d4f26d5
Nameless8243 <119530613+Nameless8243@users.noreply.github.com>
2024-01-29 16:58:18 +0100

    Finished

--- send_email.py
+++ send_email.py
@@ -1,12 +1,12 @@
 import smtplib, ssl
-
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "kalmartibor8243@gmail.com"
-    password = "vcjx wycm uqyr lyuh"
+    password = os.getenv("PASSWORD")
 
     receiver = "kalmartibor8243@gmail.com"
     context = ssl.create_default_context()
